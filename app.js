96ec5f677fd0c68117ce2df8c84efa93173a53b5
xueyan <348352046@qq.com>
2019-12-25 10:51:27 +0800

    :art:首次上传

---
+++ app.js
@@ -0,0 +1,34 @@
+import {
+  get
+} from './utils/request'
+import api from './utils/urls'
+
+App({
+  onLaunch: function () {
+    this.getOpenId()
+  },
+  // AppID(小程序ID):wx6f18657f1441b123    AppSecret(小程序密钥):3cef2fc89ceb27c0b912557a39776dd1
+  getOpenId() {
+    const requestData = {
+      appid: 'wx6f18657f1441b123',
+      secret: '3cef2fc89ceb27c0b912557a39776dd1'
+    }
+    const openid = wx.getStorageSync('openid')
+    if (!openid) {
+      wx.login({
+        success: res => {
+          get(`${api.getOpenIdUrl}?appid=${requestData.appid}&secret=${requestData.secret}&grant_type=authorization_code&js_code=${res.code}`).then(res => {
+            wx.setStorageSync('openid', res.openid)
+          })
+        },
+        fail: () => {},
+        complete: () => {}
+      });
+    }
+  },
+
+
+  globalData: {
+    openid:''
+  }
+})
---
+++ utils/urls.js
@@ -0,0 +1,9 @@
+const BASEURL = 'http://127.0.0.1:18080/'
+
+module.exports = {
+//获取openId
+getOpenIdUrl:`https://api.weixin.qq.com/sns/jscode2session`,
+//获取首页列表数据
+getResumeUrl:`${BASEURL}/resume/getresume`
+
+}
