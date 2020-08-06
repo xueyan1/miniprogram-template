e2a9bcd9bec86dfe9489e1e4ba24420d3f232ccc
薛严 <xueyan@guoyanbodeMacBook-Pro.local>
2020-08-06 11:34:39 +0800

    :art:删除无用代码

--- app.js
+++ app.js
@@ -3,35 +3,10 @@ import reducer from './store/reducer'
 import {
   Provider
 } from './redux/mp-redux'
-const store = createStore(reducer)
-import {
-  get
-} from './utils/request'
-import api from './utils/urls'
 
 App(
   Provider(store)({
-    onLaunch: function () {
-      this.getOpenId()
-    },
-    // AppID(小程序ID):wx6f18657f1441b123    AppSecret(小程序密钥):3cef2fc89ceb27c0b912557a39776dd1
-    getOpenId() {
-      const requestData = {
-        appid: 'wx6f18657f1441b123',
-        secret: '3cef2fc89ceb27c0b912557a39776dd1'
-      }
-      const openid = wx.getStorageSync('openid')
-      if (!openid) {
-        wx.login({
-          success: res => {
-            get(`${api.getOpenIdUrl}?appid=${requestData.appid}&secret=${requestData.secret}&grant_type=authorization_code&js_code=${res.code}`).then(res => {
-              wx.setStorageSync('openid', res.openid)
-            })
-          },
-          fail: () => { },
-          complete: () => { }
-        });
-      }
+    onLaunch () {
     },
     globalData: {
       openid: ''
--- utils/config/env.config.js
+++ utils/config/env.config.js
@@ -16,6 +16,3 @@ const config = {
 }
 
 module.exports = config
-
-// appId: wxe22fcf1338d42583 生产环境
-// appId: wx1807449059560cc0 测试环境
